4ccaace312d07ff5ccf858eb09379389f4c43811
Büşra Arıgün <busrarigun@gmail.com>
2023-02-22 01:12:15 +0300

    LaunchCard revised

--- src/components/LaunchCard.tsx
+++ src/components/LaunchCard.tsx
@@ -1,6 +1,5 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
-import axios from 'axios';
 
 interface Props {
   data: {
@@ -11,11 +10,38 @@ interface Props {
 }
 const LaunchCard = (props: Props) => {
   return (
-    <TouchableOpacity onPress={() => props.onPress && props.onPress()}>
-      <Text>name: {props.data.name}</Text>
-      <Text>details: {props.data.details}</Text>
+    <TouchableOpacity style={styles.container} onPress={() => props.onPress && props.onPress()}>
+      <Text style={styles.name}>{props.data.name}</Text>
+      <Text style={styles.details}>{props.data.details ? props.data.details: 'No details'}</Text>
     </TouchableOpacity>
   );
 };
 
 export default LaunchCard;
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor:'#E4E7EC',
+    borderWidth:1,
+    minHeight:100,
+    margin:10,
+    paddingHorizontal:10,
+    paddingVertical:8,
+    borderRadius:8,
+    shadowOffset: {width: 0.5, height: 2},  
+    shadowColor: '#171717',  
+    shadowOpacity: 0.2,  
+    shadowRadius: 3, 
+  },
+  name:{
+    fontWeight:'500',
+    fontSize:20,
+    paddingHorizontal:15,
+    paddingVertical:8,
+  },
+  details:{
+    paddingHorizontal:15,
+    paddingVertical:8,
+    color:'#667085'
+  }
+});
