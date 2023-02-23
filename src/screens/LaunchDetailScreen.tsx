aac121452f85cc5d3d5504f2823195466591e046
Büşra Arıgün <busrarigun@gmail.com>
2023-02-23 22:45:57 +0300

    back button added to LaunchDetailScreen

--- src/screens/LaunchDetailScreen.tsx
+++ src/screens/LaunchDetailScreen.tsx
@@ -6,12 +6,15 @@ import {
   TouchableOpacity,
   Dimensions,
   Linking,
+  StyleSheet,
 } from 'react-native';
 import React from 'react';
 import YoutubePlayer from '../components/YoutubePlayer';
+import {Colors} from '../resources/Colors';
 
 interface Props {
   route?: any;
+  navigation?: any;
 }
 
 const LaunchDetailScreen = (props: Props) => {
@@ -22,23 +25,23 @@ const LaunchDetailScreen = (props: Props) => {
   };
 
   return (
-    <View style={{flex: 1, paddingHorizontal: 15, backgroundColor: '#fff'}}>
-      <Text style={{textAlign: 'center', fontSize: 20, marginVertical: 50}}>
-        Launch Detail
-      </Text>
-      <View style={{alignItems:'center'}}>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={{left: 0, position: 'absolute'}}
+          onPress={() => props.navigation.goBack()}>
+          <Text style={{fontSize: 25}}>←</Text>
+        </TouchableOpacity>
+
+        <Text style={{textAlign: 'center', fontSize: 20}}>Launch Detail</Text>
+      </View>
+      <View style={{alignItems: 'center'}}>
         <YoutubePlayer
           videoId={props.route.params.item.links.youtube_id}
           height={Dimensions.get('window').height / 4}
           width={Dimensions.get('window').width / 1.11}
         />
-        <View
-          style={{
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            flexDirection: 'row',
-            width: '100%',
-          }}>
+        <View style={styles.body}>
           <Image
             style={{width: 78, height: 90}}
             source={{uri: props.route.params.item.links.patch.small}}
@@ -66,13 +69,37 @@ const LaunchDetailScreen = (props: Props) => {
             </View>
           </View>
         </View>
-        <TouchableOpacity style={{paddingVertical:30}} onPress={() => onPressWikiLink(props.route.params.item.links.wikipedia)}>
-        <Text>go wikipedia page</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={{paddingVertical: 30}}
+          onPress={() =>
+            onPressWikiLink(props.route.params.item.links.wikipedia)
+          }>
+          <Text>go wikipedia page</Text>
+        </TouchableOpacity>
       </View>
-  
     </View>
   );
 };
 
 export default LaunchDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  body: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '100%',
+  },
+});
